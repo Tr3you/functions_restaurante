3adaed72f223523e9fa60944425eeb99912c33b3
Erick Ruiz <eruisacosta@gmail.com>
2021-05-07 17:24:32 -0500

    funcion añadir plato terminada

---
+++ HttpTrigger2/__init__.py
@@ -0,0 +1,33 @@
+import logging
+
+import azure.functions as func
+from .add_plate import create_plate_id, add_plate
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    restaurant_id = req.params.get('restaurant_id')
+    plate_name=req.params.get('plate_name')
+    plate_price=int(req.params.get('plate_price'))
+    plate_category=req.params.get('plate_category')
+    if not restaurant_id or plate_name or plate_price or plate_category:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            restaurant_id = req.params.get('restaurant_id')
+            plate_name=req.params.get('plate_name')
+            plate_price=int(req.params.get('plate_price'))
+            plate_category=req.params.get('plate_category')
+
+    if restaurant_id and plate_name and plate_price and plate_category:
+        plate_id = create_plate_id()
+        response_add_plate = add_plate(restaurant_id, plate_id, plate_name, plate_price, plate_category)
+        return func.HttpResponse(response_add_plate)
+    else:
+        return func.HttpResponse(
+             "ERROR - Alguno de los parametros necesarios esta vacio!",
+             status_code=200
+        )
---
+++ HttpTrigger2/db.py
@@ -0,0 +1,25 @@
+platesDb = [
+    {
+        'restaurant_id': 34567,
+        'menu': [
+            {
+                'plate_id': 101,
+                'plate_name': 'chuleta valluna',
+                'price':54000,
+                'plate_category':'plato fuerte'
+            }
+        ]
+    },
+
+    {
+        'restaurant_id': 78903,
+        'menu': [
+            {
+                'plate_id': 201,
+                'plate_name': 'Ensalada Cesar',
+                'price':28000,
+                'plate_category':'entrnate'
+            }
+        ]
+    }
+]
